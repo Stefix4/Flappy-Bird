e56ff93ef402a356c696fbf667437ce0a10b741a
Stef <94927709+Stefix4@users.noreply.github.com>
2023-06-02 10:53:38 +0300

    Add files via upload

--- Flappy Bird/src/main.cpp
+++ Flappy Bird/src/main.cpp
@@ -3,19 +3,34 @@
 int main(void)
 {
 
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 900;
+    const int screenHeight = 500;
+    bool isFullscreen = false;
+
+    Rectangle fullscreenButton = { 10, 10, 120, 40 };
 
     InitWindow(screenWidth,screenHeight, "Flappy Bird");
 
-
+    Image logo = LoadImage("../resources/flappy-bird-logo.png");
     Texture2D fbf_tx = LoadTexture("../resources/flappy-bird-flaps.png");// fbf_tx = flappy bird flaps texture
     Texture2D background = LoadTexture("../resources/background.png");
+    SetWindowIcon(logo);
+    SetWindowMinSize(screenWidth,screenHeight);
     
     SetTargetFPS(144);               
 
     while (!WindowShouldClose())    
     {
+        if (IsKeyPressed(KEY_F11) || CheckCollisionPointRec(GetMousePosition(), fullscreenButton) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)){
+            isFullscreen = !isFullscreen;
+        
+        if(isFullscreen)
+            ToggleFullscreen();
+        else{
+            SetWindowSize(screenWidth, screenHeight);
+            ToggleFullscreen();
+        }
+        }
 
         BeginDrawing();
 
