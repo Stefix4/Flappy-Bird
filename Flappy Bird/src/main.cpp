a310fd2e6f2c01f439534a96a76d5b899952b0ce
Stef <94927709+Stefix4@users.noreply.github.com>
2023-06-02 14:26:48 +0300

    Add files via upload

--- Flappy Bird/src/main.cpp
+++ Flappy Bird/src/main.cpp
@@ -13,8 +13,9 @@ int main(void)
     SetTargetFPS(144); 
 
     Image logo = LoadImage("../resources/flappy-bird-logo.png");
-    Texture2D fbf = LoadTexture("../resources/flappy-bird-flaps.png");// fbf = flappy bird flaps
+    Texture2D fb = LoadTexture("../resources/flappy-bird-photo-removebg-preview.png");// fb = flappy bird 
     Texture2D background = LoadTexture("../resources/background.png");
+    Texture2D pill1 = LoadTexture("../resources/pillar-1.png");
     SetWindowIcon(logo);
     SetWindowMinSize(screenWidth,screenHeight);
     HideCursor();
@@ -37,8 +38,8 @@ int main(void)
 
             ClearBackground(RAYWHITE);
             DrawTexture(background,0,0,WHITE);
-            DrawTexture(fbf,screenWidth/2,screenHeight/2,WHITE);
-            
+            DrawTexture(fb,screenWidth/2,screenHeight/2,WHITE);
+            DrawTexture(pill1,screenWidth/2,screenHeight/2,WHITE);
 
         EndDrawing();
 
